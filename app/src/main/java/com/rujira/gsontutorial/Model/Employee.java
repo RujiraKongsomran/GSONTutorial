7f39ea70223270bc4ea26e9bf4d49c03c54e4236
Rujira Kongsomran <biwberry@outlook.com>
2020-02-12 22:14:07 +0700

    Create Model

---
+++ app/src/main/java/com/rujira/gsontutorial/Model/Employee.java
@@ -0,0 +1,13 @@
+package com.rujira.gsontutorial.Model;
+
+public class Employee {
+    private String firstName;
+    private int age;
+    private String mail;
+
+    public Employee(String firstName, int age, String mail) {
+        this.firstName = firstName;
+        this.age = age;
+        this.mail = mail;
+    }
+}
