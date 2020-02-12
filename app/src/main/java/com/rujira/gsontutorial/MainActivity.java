0ce3e2f711500622aca3ae6684f05c63ced623c6
Rujira Kongsomran <biwberry@outlook.com>
2020-02-12 22:19:40 +0700

    Convert object to Json String

--- app/src/main/java/com/rujira/gsontutorial/MainActivity.java
+++ app/src/main/java/com/rujira/gsontutorial/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+import com.rujira.gsontutorial.Model.Employee;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Gson gson = new Gson();
+
+        Employee employee = new Employee("John", 30, "john@gmail.com");
+        String json = gson.toJson(employee);
     }
 }
