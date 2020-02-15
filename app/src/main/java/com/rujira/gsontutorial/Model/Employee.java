519b860a05a87d9e0abf8ca0cfa7f047a25c198d
Rujira Kongsomran <biwberry@outlook.com>
2020-02-15 20:30:45 +0700

    Add List<FamilyMember> mFamily in class Employee

--- app/src/main/java/com/rujira/gsontutorial/Model/Employee.java
+++ app/src/main/java/com/rujira/gsontutorial/Model/Employee.java
@@ -2,6 +2,8 @@ package com.rujira.gsontutorial.Model;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
 public class Employee {
     @SerializedName("first_name")
     private String mFirstName;
@@ -12,10 +14,14 @@ public class Employee {
     @SerializedName("address")
     private Address mAddress;
 
-    public Employee(String mFirstName, int mAge, String mMail, Address mAddress) {
+    @SerializedName("family")
+    private List<FamilyMember> mFamily;
+
+    public Employee(String mFirstName, int mAge, String mMail, Address mAddress, List<FamilyMember> mFamily) {
         this.mFirstName = mFirstName;
         this.mAge = mAge;
         this.mMail = mMail;
         this.mAddress = mAddress;
+        this.mFamily = mFamily;
     }
 }
