37574e8d747ec70ee43b7a79a4e9f1f2c10a6d5d
Rujira Kongsomran <biwberry@outlook.com>
2020-02-15 20:49:53 +0700

    Convert object to Json String in case List<FamilyMember> family variable

--- app/src/main/java/com/rujira/gsontutorial/MainActivity.java
+++ app/src/main/java/com/rujira/gsontutorial/MainActivity.java
@@ -21,34 +21,34 @@ public class MainActivity extends AppCompatActivity {
 
         Gson gson = new Gson();
 
-//        List<FamilyMember> family = new ArrayList<>();
-//        family.add(new FamilyMember("Wife", 30));
-//        family.add(new FamilyMember("Daugther", 5));
-//
-//        Address address = new Address("Germany", "Berlin");
-//
-//        Employee employee = new Employee("John", 30, "john@gmail.com", address, family);
-//        String json = gson.toJson(employee);
+        List<FamilyMember> family = new ArrayList<>();
+        family.add(new FamilyMember("Wife", 30));
+        family.add(new FamilyMember("Daugther", 5));
 
-        String json = "{\n" +
-                "  \"address\": {\n" +
-                "    \"city\": \"Berlin\",\n" +
-                "    \"country\": \"Germany\"\n" +
-                "  },\n" +
-                "  \"age\": 30,\n" +
-                "  \"family\": [\n" +
-                "    {\n" +
-                "      \"age\": 30,\n" +
-                "      \"role\": \"Wife\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"age\": 5,\n" +
-                "      \"role\": \"Daugther\"\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"first_name\": \"John\",\n" +
-                "  \"mail\": \"john@gmail.com\"\n" +
-                "}";
-        Employee employee = gson.fromJson(json, Employee.class);
+        Address address = new Address("Germany", "Berlin");
+
+        Employee employee = new Employee("John", 30, "john@gmail.com", address, family);
+        String json = gson.toJson(family);
+
+//        String json = "{\n" +
+//                "  \"address\": {\n" +
+//                "    \"city\": \"Berlin\",\n" +
+//                "    \"country\": \"Germany\"\n" +
+//                "  },\n" +
+//                "  \"age\": 30,\n" +
+//                "  \"family\": [\n" +
+//                "    {\n" +
+//                "      \"age\": 30,\n" +
+//                "      \"role\": \"Wife\"\n" +
+//                "    },\n" +
+//                "    {\n" +
+//                "      \"age\": 5,\n" +
+//                "      \"role\": \"Daugther\"\n" +
+//                "    }\n" +
+//                "  ],\n" +
+//                "  \"first_name\": \"John\",\n" +
+//                "  \"mail\": \"john@gmail.com\"\n" +
+//                "}";
+//        Employee employee = gson.fromJson(json, Employee.class);
     }
 }
