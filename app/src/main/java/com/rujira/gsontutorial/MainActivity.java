ba0b87d6c3cb41a2d33f7914d6993fe6e2db3b0a
Rujira Kongsomran <biwberry@outlook.com>
2020-02-15 20:35:32 +0700

    Convert object to Json String in case ARRAYS AND LISTS

--- app/src/main/java/com/rujira/gsontutorial/MainActivity.java
+++ app/src/main/java/com/rujira/gsontutorial/MainActivity.java
@@ -7,6 +7,10 @@ import android.os.Bundle;
 import com.google.gson.Gson;
 import com.rujira.gsontutorial.Model.Address;
 import com.rujira.gsontutorial.Model.Employee;
+import com.rujira.gsontutorial.Model.FamilyMember;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,20 +21,24 @@ public class MainActivity extends AppCompatActivity {
 
         Gson gson = new Gson();
 
-//        Address address = new Address("Germany", "Berlin");
-//
-//        Employee employee = new Employee("John", 30, "john@gmail.com", address);
-//        String json = gson.toJson(employee);
+        List<FamilyMember> family = new ArrayList<>();
+        family.add(new FamilyMember("Wife", 30));
+        family.add(new FamilyMember("Daugther", 5));
 
-        String json = "{\n" +
-                "  \"address\": {\n" +
-                "    \"city\": \"Berlin\",\n" +
-                "    \"country\": \"Germany\"\n" +
-                "  },\n" +
-                "  \"age\": 30,\n" +
-                "  \"first_name\": \"John\",\n" +
-                "  \"mail\": \"john@gmail.com\"\n" +
-                "}";
-        Employee employee = gson.fromJson(json, Employee.class);
+        Address address = new Address("Germany", "Berlin");
+
+        Employee employee = new Employee("John", 30, "john@gmail.com", address, family);
+        String json = gson.toJson(employee);
+
+//        String json = "{\n" +
+//                "  \"address\": {\n" +
+//                "    \"city\": \"Berlin\",\n" +
+//                "    \"country\": \"Germany\"\n" +
+//                "  },\n" +
+//                "  \"age\": 30,\n" +
+//                "  \"first_name\": \"John\",\n" +
+//                "  \"mail\": \"john@gmail.com\"\n" +
+//                "}";
+//        Employee employee = gson.fromJson(json, Employee.class);
     }
 }
